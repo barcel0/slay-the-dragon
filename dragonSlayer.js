e001e134b2af3702001992aec0de8ddc5b2cc64f
barcel0 <lbarcelocarrera@gmail.com>
2017-01-17 21:25:23 +0000

    dragonLife added, more info showed, dmg points randomized

--- dragonSlayer.js
+++ dragonSlayer.js
@@ -1,22 +1,27 @@
 //theOdinProject Javascrip Codecademy Slay the dragon excercise
-//v1.0
-
+//v2.0
+//dragonLife added, more info showed in console, damage points randomized
 var slaying = true;
-var youHit = Math.floor(Math.random()*2);
-var dmgThisRound = Math.folor(Math.random()*5+1);
-var totalDamage = 0;
-
+var youHit = Math.floor(Math.random()*4);
+var dmgThisRound = Math.floor(Math.random()*5+1);
+var dragonLife = 40;
+console.log("A wild dragon showed up! He's got " + dragonLife + " life points. Good luck!");
 
 while(slaying){
-    if(youHit = 1){
+    if(youHit >= 1){
         console.log("You hit the dragon! Making " + dmgThisRound + " damage points.");
-        totalDamage += dmgThisRound;
-            if(totalDamage>=dragonLife){
+        
+        dragonLife -= dmgThisRound;
+        console.log("Dragon life: " + dragonLife + " points.");
+            if(dragonLife<=0){
                 console.log("Dragon died.");
                 slaying=false;
                 }
             else{
                 youHit = Math.floor(Math.random()*5+1);
+                    if(youHit>=2){
+                        dmgThisRound = Math.floor(Math.random()*5+1);
+                        }
                 }
         
         }
@@ -24,6 +29,11 @@ while(slaying){
         console.log("You missed, the dragon just fucked you in the arse.");
         slaying=false;
         }
+    
+    
+    
+    
+    
     
     
     }
